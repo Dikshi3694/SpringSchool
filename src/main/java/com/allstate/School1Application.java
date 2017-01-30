3da525973e66154e854c1377059fe4c8e1090bb0
ARS <sample@gmail.com>
2017-01-30 09:19:20 +0530

    initial commit

---
+++ src/main/java/com/allstate/School1Application.java
@@ -0,0 +1,12 @@
+package com.allstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School1Application.class, args);
+	}
+}
